e4291886649ebb1864b1372270a864e760e1a698
daxgnguyen <152653657+daxgnguyen@users.noreply.github.com>
2026-01-26 05:26:05 +0000

    Fixed something

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ st.title('IST 488 Labs')
 Lab1 = st.Page('labs/lab1.py', title = 'Lab 1', icon = '📝')
 Lab2 = st.Page('labs/lab2.py', title = 'Lab 2', icon = '📝')
 pg = st.navigation([Lab1, Lab2])
-st.st_page_config(page_title = 'IST 488 Lasbs'
+st.st_page_config(page_title = 'IST 488 Lasbs',
                   initial_sidebar_state = 'expanded')
 pg.run()
