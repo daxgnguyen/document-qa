eb583fa9d6238fc8a06b80ce634718e713d94dc5
daxgnguyen <152653657+daxgnguyen@users.noreply.github.com>
2026-01-30 00:59:11 +0000

    Fixed lab ordering

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ st.set_page_config(page_title = 'IST 488 Labs',
                   initial_sidebar_state = 'expanded')
 
 st.title('IST 488 Labs')
-Lab2 = st.Page('labs/lab2.py', title = 'Lab 2', icon = '📝')
 Lab1 = st.Page('labs/lab1.py', title = 'Lab 1', icon = '📝')
-pg = st.navigation([Lab1, Lab2])
+Lab2 = st.Page('labs/lab2.py', title = 'Lab 2', icon = '📝')
+pg = st.navigation([Lab2, Lab1])
 
 pg.run()
