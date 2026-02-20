b899074f45636228bac877cb545a536408808ae0
daxgnguyen <152653657+daxgnguyen@users.noreply.github.com>
2026-02-20 02:39:45 +0000

    fixed argument funciton

--- labs/lab5.py
+++ labs/lab5.py
@@ -70,7 +70,7 @@ if go:
 
     if msg.tool_calls:
         tc = msg.tool_calls[0]
-        args = json.loads(tc.arguments)
+        args = json.loads(tc.function.arguments)
         weather = get_current_weather(args['location'], st.secrets["weather_api_key"])
         st.json(weather)
 
